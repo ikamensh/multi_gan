af1a3b80907181c73cde11cc87e1b21dad36c718
ikkamens <ikkamens@amazon.com>
2019-07-30 20:45:36 +0200

    Initial commit

---
+++ first.py
@@ -0,0 +1,141 @@
+import glob
+import imageio
+
+from ilya_ezplot.plot.plot import plt
+import numpy as np
+import os
+import PIL
+from tensorflow.python.keras import layers
+import time
+
+import tensorflow as tf
+
+
+(train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
+
+train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+train_images = (train_images - 127.5) / 127.5 # Normalize the images to [-1, 1]
+
+BUFFER_SIZE = 60000
+BATCH_SIZE = 256
+
+# Batch and shuffle the data
+train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+
+from generator import make_generator_model
+g = make_generator_model()
+
+noise = tf.random.normal([1, 100])
+generated_image = g(noise, training=False)
+
+imageio.imsave("naja.png", generated_image.numpy()[0])
+
+from discriminator import make_discriminator_model
+
+d = make_discriminator_model()
+decision = d(generated_image)
+print (decision)
+
+
+# This method returns a helper function to compute cross entropy loss
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+
+
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                 discriminator_optimizer=discriminator_optimizer,
+                                 generator=g,
+                                 discriminator=d)
+
+EPOCHS = 50
+noise_dim = 100
+num_examples_to_generate = 16
+
+# We will reuse this seed overtime (so it's easier)
+# to visualize progress in the animated GIF)
+seed = tf.random.normal([num_examples_to_generate, noise_dim])
+
+# Notice the use of `tf.function`
+# This annotation causes the function to be "compiled".
+@tf.function
+def train_step(images):
+    noise = tf.random.normal([BATCH_SIZE, noise_dim])
+
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+      generated_images = g(noise, training=True)
+
+      real_output = d(images, training=True)
+      fake_output = d(generated_images, training=True)
+
+      gen_loss = generator_loss(fake_output)
+      disc_loss = discriminator_loss(real_output, fake_output)
+
+    gradients_of_generator = gen_tape.gradient(gen_loss, g.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(disc_loss, d.trainable_variables)
+
+    generator_optimizer.apply_gradients(zip(gradients_of_generator, g.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, d.trainable_variables))
+
+
+def train(dataset, epochs):
+    for epoch in range(epochs):
+        start = time.time()
+
+        for image_batch in dataset:
+            train_step(image_batch)
+
+        generate_and_save_images(g, epoch + 1, seed)
+
+        if (epoch + 1) % 15 == 0:
+            checkpoint.save(file_prefix = checkpoint_prefix)
+
+        print (f'Time for epoch {(epoch + 1)} is {(time.time() - start):.2f} sec')
+
+    # Generate after the final epoch
+    generate_and_save_images(g, epochs, seed)
+
+def generate_and_save_images(model, epoch, test_input):
+  # Notice `training` is set to False.
+    # This is so all layers run in inference mode (batchnorm).
+    predictions = model(test_input, training=False)
+
+    fig = plt.figure(figsize=(4, 4))
+
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i + 1)
+        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+        plt.axis('off')
+
+    plt.savefig(f'image_at_epoch_{epoch:04d}.png')
+
+train(train_dataset, 5)
+
+# anim_file = 'dcgan.gif'
+#
+# with imageio.get_writer(anim_file, mode='I') as writer:
+#   filenames = glob.glob('image*.png')
+#   filenames = sorted(filenames)
+#   last = -1
+#   for i,filename in enumerate(filenames):
+#     frame = 2*(i**0.5)
+#     if round(frame) > round(last):
+#       last = frame
+#     else:
+#       continue
+#     image = imageio.imread(filename)
+#     writer.append_data(image)
+#   image = imageio.imread(filename)
+#   writer.append_data(image)
