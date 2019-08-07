8f962f9429e2333c5f7f1869a36b1dd894119586
Ilya <ikamenshchikov@gmail.com>
2019-08-07 10:22:19 +0200

    batchnorm for discr

--- generator/net.py
+++ generator/net.py
@@ -3,7 +3,11 @@ import tensorflow as tf
 
 def make_generator_model(noise_dim, num_classes, color_ch=3):
     cnn = tf.keras.Sequential()
-    cnn.add(layers.Dense(7*7*128, use_bias=False, input_shape=(noise_dim,)))
+    cnn.add(layers.Dense(7*7*256, input_shape=(noise_dim,)))
+    cnn.add(layers.BatchNormalization())
+    cnn.add(layers.LeakyReLU())
+
+    cnn.add(layers.Dense(7*7*128, input_shape=(noise_dim,)))
     cnn.add(layers.BatchNormalization())
     cnn.add(layers.LeakyReLU())
 
@@ -17,14 +21,22 @@ def make_generator_model(noise_dim, num_classes, color_ch=3):
 
     cnn.add(layers.Conv2DTranspose(256, (1, 1), strides=(1, 1), padding='same', use_bias=False))
     assert cnn.output_shape == (None, 7, 7, 256), cnn.output_shape
+    cnn.add(layers.BatchNormalization())
+    cnn.add(layers.LeakyReLU())
 
     cnn.add(layers.Conv2DTranspose(64, (4, 4), strides=(2, 2), padding='valid', use_bias=False))
     assert cnn.output_shape == (None, 16, 16, 64), cnn.output_shape
     cnn.add(layers.BatchNormalization())
     cnn.add(layers.LeakyReLU())
 
-    cnn.add(layers.Conv2DTranspose(color_ch, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+    cnn.add(layers.Conv2DTranspose(256, (1, 1), strides=(1, 1), padding='same', use_bias=False))
+    assert cnn.output_shape == (None, 16, 16, 256), cnn.output_shape
+    cnn.add(layers.BatchNormalization())
+    cnn.add(layers.LeakyReLU())
+
+    cnn.add(layers.Conv2DTranspose(color_ch, (5, 5), strides=(2, 2), padding='same', activation='tanh'))
     assert cnn.output_shape == (None, 32, 32, color_ch), cnn.output_shape
+
 
     latent = layers.Input(shape=(noise_dim, ))
 
@@ -36,9 +48,20 @@ def make_generator_model(noise_dim, num_classes, color_ch=3):
 
     h = layers.multiply([latent, cls])
 
+
+
+
     fake_image = cnn(h)
 
-    generator = tf.keras.Model([latent, image_class], fake_image)
+    corrector = tf.keras.Sequential()
+    corrector.add(layers.Flatten(input_shape=(32,32,color_ch)))
+    corrector.add(layers.Dense(32*32*color_ch))
+    corrector.add(layers.Reshape((32,32,color_ch)))
+    summ = layers.Add()([corrector(fake_image), fake_image])
+    corrected = layers.Activation(activation='tanh') (summ)
+
+
+    generator = tf.keras.Model([latent, image_class], corrected)
     assert generator.output_shape == (None, 32, 32, color_ch), generator.output_shape
 
     return generator
